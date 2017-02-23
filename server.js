9c8f8611e4397f32ed4a67d8ed346ffdfe92f01e
Joshua Lee <joshualee@iconfigure.org>
2017-02-22 22:07:21 -1000

    updated server and can now authenticate

--- server.js
+++ server.js
@@ -3,14 +3,57 @@ const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars');
 const galleryRouter = require('./galleryRouter.js')
 const methodOverride = require('method-override')
+
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session)
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const CONFIG = require('./config/config.json');
 const app = express();
+
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extend: true}));
   app.use(methodOverride('_method'));
-  app.use('/gallery', galleryRouter)
+  app.use(session({
+  secret: CONFIG.SESSION_SECRET,
+  store: new RedisStore()
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use('/secret', isAuthenticated);
+  app.use('/gallery', galleryRouter);
+
+ const authenticate = (username, password) => {
+  // queries user data from the DB
+ return Auth.findOne({where: {username: username, password: password}});
+
+};
+
+passport.use(new LocalStrategy(
+   function (username, password, done) {
+  console.log('hey username, password: ', username, password);
+  //returns results of query using promises and passes it into be checked
+      authenticate(username, password)
+        .then( username => {
+          return done(null, username);
+        });
+
+   }
+));
+
+passport.serializeUser(function(user, done) {
+  return done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  return done(null, user);
+});
+
 
 const db = require('./models');
   const { Gallery } = db;
+  const { Auth } = db;
 
   const hbs = handlebars.create({
       extname: '.hbs',
@@ -28,8 +71,42 @@ const db = require('./models');
     });
   })
 
+  //authentication beyon this point
+
+  function isAuthenticated (req, res, next) {
+    if(req.isAuthenticated()){
+      next();
+    }else{
+      console.log('NO i havent');
+      res.redirect('/login');
+    }
+  }
+
+  app.post('/login', passport.authenticate('local',
+
+      {successRedirect: '/',
+      failureRedirect: '/login'}));
+
+  app.get('/secret', function (req, res) {
+    res.render('./secret')
+  });
 
 
-app.listen(3000, function() {
-  db.sequelize.sync();
-});
+  app.get('/login', function (req, res) {
+    res.render('./login')
+  });
+
+  // app.post('/login', function(req, res) {
+  //   Auth.create({
+  //     username: req.body.username,
+  //     password: req.body.password,
+  //     })
+  //     .then(function (gallery) {
+  //       res.redirect('/');
+  //     });
+  // });
+
+
+  app.listen(3000, function() {
+    db.sequelize.sync();
+  });
